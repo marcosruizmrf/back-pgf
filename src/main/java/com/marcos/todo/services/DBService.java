723acd83a6a925df3aab47be9deeac92801d13c9
Marcos Ruiz Freire <marcosruizmrf@outlook.com>
2021-11-10 00:38:45 -0300

    fix: Fix the instantiation

--- src/main/java/com/marcos/todo/services/DBService.java
+++ src/main/java/com/marcos/todo/services/DBService.java
@@ -19,7 +19,7 @@ public class DBService {
 	public void instanciaBaseDeDados() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Todo t1 = new Todo(null, "Aruela Especial", "Cliente X. Obs: Ag Têmpera", sdf.parse("25/03/2022"), false);
+		Todo t1 = new Todo(null, "Arruela Especial", "Cliente X. Obs: Ag Têmpera", sdf.parse("25/03/2022"), false);
 		Todo t2 = new Todo(null, "Biela Simétrica", "Cliente Y. Obs: Ag Têmpera", sdf.parse("22/03/2021"), true);
 
 		todoRepository.saveAll(Arrays.asList(t1, t2));
